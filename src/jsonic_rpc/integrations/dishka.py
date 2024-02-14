94cbc30b0b14d09afe876ea368287aacb3c60186
dalerzafarovich <zdaler014@gmail.com>
2024-02-14 16:55:19 +0500

    fix async support for dishka injector

--- src/jsonic_rpc/integrations/dishka.py
+++ src/jsonic_rpc/integrations/dishka.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from inspect import Parameter, signature
 from typing import Annotated, Any, get_args, get_origin
 
-from dishka import Container
+from dishka import AsyncContainer, Container
 from dishka.integrations.base import wrap_injection
 
 from jsonic_rpc._internal.abstractions.di import BaseDiInjector, Depends
@@ -35,24 +35,36 @@ def default_parse_dependency(
 
 
 @dataclass
-class Context:
-    container: Container
+class ContainersWrapper:
+    sync_container: Container | None = None
+    async_container: AsyncContainer | None = None
+
+    def __post_init__(self):
+        if not (self.sync_container or self.async_container):
+            raise TypeError(
+                "Neither sync_container nor async_container specifyied in "
+                "Context constructor"
+            )
 
 
-class DiInjector(BaseDiInjector[Context]):
-    def __init__(self, container: Container):
-        self._container = container
+class DiInjector(BaseDiInjector[ContainersWrapper]):
+    def __init__(self, context: ContainersWrapper):
+        self._sync_container = context.sync_container
+        self._async_container = context.async_container
 
     def call_injected(
         self,
         method: RegisteredMethod,
         loader: BaseLoader,
         params: Params,
-        context: Context | None,
+        context: ContainersWrapper | None,
     ) -> Result:
         non_dep_args = method_non_depends_args(signature(method.origin))
         loaded_args = loader.load_args(method, non_dep_args, params)
-        container = context.container if context else self._container
+        container = context.sync_container if context else self._sync_container
+
+        if not container:
+            raise TypeError("Sync container is not specified")
 
         with container({MethodArgs: loaded_args}) as subcontainer:
             injected = wrap_injection(
@@ -67,17 +79,23 @@ class DiInjector(BaseDiInjector[Context]):
         method: RegisteredMethod,
         loader: BaseLoader,
         params: Params,
-        context: Context | None,
+        context: ContainersWrapper | None,
     ) -> RegisteredMethod:
         non_dep_args = method_non_depends_args(signature(method.origin))
         loaded_args = loader.load_args(method, non_dep_args, params)
-        container = context.container if context else self._container
+        container = (
+            context.async_container if context else self._async_container
+        )
+
+        if not container:
+            raise TypeError("Async container is not specified")
 
         async with container({MethodArgs: loaded_args}) as subcontainer:
             injected = wrap_injection(
                 method.origin,
-                container_getter=lambda: subcontainer,
+                container_getter=lambda a, b: subcontainer,  # type: ignore
                 parse_dependency=default_parse_dependency,
+                is_async=True,
             )
             return await injected(
                 *loaded_args.positionals, **loaded_args.keywords
